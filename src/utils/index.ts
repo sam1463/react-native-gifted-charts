914b8d4f8968da76e5bde62f1401db1a80726df2
Davyd NRB <4661784+retyui@users.noreply.github.com>
2026-09-09 14:01:51 +0200

    Refactor React Native version handling

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,12 +1,12 @@
 import {useEffect, useRef} from 'react';
 import {Dimensions, Platform} from 'react-native';
 
-const versionString = Platform.constants.reactNativeVersion;
-
-const versionAr = versionString?.split?.('.') ?? '';
-const msb = Number(versionAr[0]);
-const mid = Number(versionAr[1]);
-const lsb = Number(versionAr[2]);
+const versionObj = Platform.constants.reactNativeVersion;
+const {
+  major: msb,
+  minor: mid,
+  patch: lsb
+} = versionObj;
 
 export const rnVersion =
   (!isNaN(msb) ? msb : 0) * 1000000 +
